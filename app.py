f359e32d628796b78744dfd93ed55fa9441abcc2
sq <wweerrbb@gmail.com>
2023-06-07 15:21:53 -0400

    adding pickle lib to store the knowledge_base, and adding condition to safeguard re-parsing the same pdf file if already exists

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
+import pickle
 import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -8,6 +10,14 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+
+# #sidebar contents
+# with st.sidebar:
+#     st.title("ChatBot Pandora")
+#     st.markdown(''' Hi there ''')
+#     add_vertical_space(4)
+#     st.write('Made By SQ')
+
 
 def main():
     load_dotenv()
@@ -34,16 +44,26 @@ def main():
         chunks = text_splitter.split_text(text)
         
         # st.write(chunks)
-        #create embeddings - convert words into numbers and build a vector of connections(an object for similarity search and semantic search)
-        embeddings = OpenAIEmbeddings()
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        store_name = pdf.name[:-4]
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                knowledge_base = pickle.load(f)
+                # st.write("Embeddings Loaded from the Disk")
+        else:
+            #create embeddings - convert words into numbers and build a vector of connections(an object for similarity search and semantic search)
+            embeddings = OpenAIEmbeddings()
+            knowledge_base = FAISS.from_texts(chunks, embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(knowledge_base, f)
 
         user_question = st.text_input("What do you want to know about from your PDF?")
         if user_question:
-            docs = knowledge_base.similarity_search(user_question)
+            docs = knowledge_base.similarity_search(user_question, k=5)
             # st.write(docs)
             #chose a llm
-            llm = OpenAI()
+            llm = OpenAI(temperature=.2)
             chain = load_qa_chain(llm, chain_type="stuff")
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=user_question)
